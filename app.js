949782cdc6ed2ab92cf2305977553d881f3b9cb0
mylcevaleksandr <myltsevaleksandr@gmail.com>
2024-02-23 22:27:51 +0300

    renamed frontend file and npm now doesn't bug

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ MongoDBConnection.getConnection((error, connection) => {
     const app = express();
 
     app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.static(path.join(__dirname, 'frontend/dist/online-store')));
+    app.use(express.static(path.join(__dirname, 'Online_Store/dist/online-store')));
 
     app.use(express.json());
     app.use(cors({credentials: true, origin: true}));
